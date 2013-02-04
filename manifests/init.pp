93c0a44872baf72661e92b77f1141d365757e447
Eslam El Husseiny <emamdouh@emamdouh.sw.in>
2013-02-04 13:43:50 +0200

    adding exec resource to install brew command.

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+class puppet-homebrew{
+  Exec{
+    path => '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin',
+  }
+  exec{'install homebrew':
+    command => 'ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"'
+    creates => '/usr/local/bin/brew'
+  }
+}
